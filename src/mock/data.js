493b25d5442eb34530fb47717f80f0ccb31882e1
nexttoonbox <72653436+nexttoonbox@users.noreply.github.com>
2020-11-30 21:48:28 +0300

    Update data.js

--- src/mock/data.js
+++ src/mock/data.js
@@ -14,15 +14,22 @@ export const heroData = {
   subtitle: 'Site under maintenance',
   cta: '',
 };
-
+// ABOUT DATA
+export const aboutData = {
+  img: 'project.jpg',
+  paragraphOne: 'Nexttoon is a website for reading and following your faviorte arabic manga and webtoon artists',
+  paragraphTwo: 'NextToon is improving and coming with new version in the next few weeks',
+  paragraphThree: 'for more information and update visit our twitter account',
+  twitter: 'https://www.nexttoon.net', // if no resume, the button will not show up
+};
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
     img: 'project.jpg',
-    title: 'Manga and webtoon',
-    info: 'Nexttoon is a website for reading and following your faviorte arabic manga and webtoon artists',
-    info2: 'NextToon is improving and coming with new version in the next few weeks',
+    title: 'NextToon V.3.0',
+    info: 'All new All improved',
+    info2: 'NextToon 3.0 will have a new homepage and new style',
     url: 'nexttoon.net',
     repo: 'https://www.nexttoon.net', // if no repo, the button will not show up
   },
